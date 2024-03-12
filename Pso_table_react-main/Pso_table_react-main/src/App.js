fd212eb7dc8ad5d249534845f7fe28a79e699318
riyal <riyaljain533@gmail.com>
2024-03-13 01:57:18 +0530

    donr

--- Pso_table_react-main/Pso_table_react-main/src/App.js
+++ Pso_table_react-main/Pso_table_react-main/src/App.js
@@ -1,18 +1,19 @@
 
 import "./App.css";
+
+
 import PsoTable from "./Screens/Psotable";
-// import PercentageTable from "./Screens/map";
+
 
 function App() {
   return (
     <>
+    
     <div className="vh-10"></div>
       <div className="container-fluid bg-secondary ">
         <PsoTable />
       </div>
-      {/* <div className="container-fluid bg-secondary ">
-        <PercentageTable />
-      </div> */}
+    
     
     </>
   );
